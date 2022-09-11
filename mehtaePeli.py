7b21364d9f8fbe1cebb9e874884c5004f4df96b3
suskikoodaa <suskeskitalo@gmail.com>
2022-09-11 17:28:09 +0300

    add weapon class, effects, weapon type

--- mehtaePeli.py
+++ mehtaePeli.py
@@ -1,9 +1,15 @@
 import random  # tarvitaan random modulea kun arvotaan metsän kuvaustekstit
 import sys
 import colorama
+from colorama import Fore, Style
 import monsterGenerator
 from player import Player
+from player import Weapon
+from player import WeaponType
+from player import SpecialEffect
 import os
+
+
 def p(s, c):
     color = {
         "black": "\u001b[30m",
@@ -14,11 +20,14 @@ def p(s, c):
         "magenta": "\u001b[35m",
         "cyan": "\u001b[36m",
         "white": "\u001b[37m",
-        "bwhite": "\u001b[37;1m"
+        "bwhite": "\u001b[37;1m",
+        "orange": Fore.LIGHTYELLOW_EX + Style.DIM
     }
     reset = "\u001b[0m"
     colorama.init()
     print(color[c] + s + reset)
+
+
 def death(msg):
     p(f"""
                                  _____  _____
@@ -29,8 +38,8 @@ def death(msg):
                                 |  | \\ | |   |
                                 |            |
                  ______.______%_|            |__________  _____
-               _/       {player_name.center(32," ")}\\|     |
-              |         {msg.center(32," ")}       <
+               _/       {player_name.center(32, " ")}\\|     |
+              |         {msg.center(32, " ")}       <
               |_____.-._________              ____/|___________|
                                 |            |
                                 |            |
@@ -49,7 +58,6 @@ def death(msg):
     # input("Press enter to continue.")
 
 
-
 player = Player(hitpoints=30, weapon_damage=1, magicpoints=0)
 
 ############################################################################
@@ -62,6 +70,7 @@ p(r"""
 """, "red")
 player_name = input("Your name? ")  # kysyy pelaajan nimen pelin alussa
 print(f"Welcome {player_name}")  # toivottaa pelaajan tervetulleeksi
+
 
 ############################################################################
 # 1. Woods
@@ -91,6 +100,7 @@ def woods():
             return True
         else:  # jos käyttäjän valinta ei osu mihinkään vaihtoehdoista tulostetaan herjaus, ja kysytään uudestaan
             print("Trying to cheat, eh?")
+
 
 ############################################################################
 # 2. Lost
@@ -125,10 +135,13 @@ def lost():
             p(woods_hints.pop(), "green")
         p(random.choice(woods_description), "yellow")  # tulostaa satunnaisen kuvaus tekstin
         print("You arrive at another crossroad.")
-        action = action + input("What direction will you go?(n, s, w, e)")  # action stringin perään laitetaan uusi suunta
+        action = action + input(
+            "What direction will you go?(n, s, w, e)")  # action stringin perään laitetaan uusi suunta
         action = action[-8:]  # action saa arvon actionin kahdeksan viimeistä merkkiä
     print("You found the way out of the woods.")
     return True
+
+
 ############################################################################
 # 3. FIGHT!
 ############################################################################
@@ -157,8 +170,15 @@ def fight(monster, player):
     monster_name = monster["name"]
     enemy_hitpoints = monster["hp"]
     enemy_max_hp = enemy_hitpoints
+    enemy_to_hit = 0.3
+    enemy_to_hit_original = enemy_to_hit
+    effect_turns = -1
+    player_to_hit_original = player.weapon.to_hit
+
     while True:
         turn_counter = turn_counter + 1
+        if effect_turns > -1:
+            effect_turns -= 1
         print(
             f"\n*** Turn {turn_counter}, You: {player.hitpoints}/{player.max_hp} hp, {monster_name}:{enemy_hitpoints}/{enemy_max_hp} hp ***")
         ### player turn
@@ -173,11 +193,15 @@ def fight(monster, player):
                 p("You already ate it all!", "cyan")
         else:
             p(f"You attack the {monster_name} in front of you", "red")
-            roll = random.randint(1, 20)
-            if roll > 10:
+            roll = random.uniform(0, 1)
+            if roll < player.weapon.to_hit:
                 print("You managed to hit!")
-                damage = random.randint(1, player.weapon_damage)
+                damage = random.randint(player.weapon.min_dmg, player.weapon.max_dmg)
                 print(f"You deal {damage} damage")
+                proc_chance = player.weapon.proc
+                if proc_chance > random.uniform(0, 1):
+                    if player.weapon.special_effect == SpecialEffect.SLOW:
+                        effect_turns = 1
                 enemy_hitpoints = enemy_hitpoints - damage
                 if enemy_hitpoints <= 0:
                     break
@@ -185,9 +209,24 @@ def fight(monster, player):
                 print("You miss.")
 
         ### enemy turn
+        if effect_turns > 0:
+            if player.weapon.special_effect == SpecialEffect.STUN:
+                p(f"{monster_name} is stunned and cannot move!")
+                continue
+            if player.weapon.special_effect == SpecialEffect.SLOW:
+                p(f"{monster_name} seems very slow!", "red")
+                enemy_to_hit = enemy_to_hit_original / 2
+                player.weapon.to_hit += .2
+        if effect_turns == 0:
+            if player.weapon.special_effect == SpecialEffect.SLOW:
+                p(f"{monster_name} seems to gain speed again!", "red")
+                enemy_to_hit = enemy_to_hit_original
+                player.weapon.to_hit = player_to_hit_original
+            if player.weapon.special_effect == SpecialEffect.STUN:
+                p(f"{monster_name} is gaining consciousness.")
         p(f"The {monster_name} attacks!", "red")
-        roll = random.randint(1, 20)
-        if roll > 14:
+        roll = random.uniform(0, 1)
+        if roll < enemy_to_hit:
             print(f"The {monster_name} hits you.")
             damage = random.randint(1, 6)
             print(f"You take {damage} damage")
@@ -202,17 +241,18 @@ def fight(monster, player):
                 return False
         else:
             print(f"The {monster_name} misses.")
+
     p(f"You have successfully slain the {monster_name}.", "red")
     return True
+
+
 
 def tappelu():
     print(f"As you exit the woods a funky looking creature cuts your path.")
     # print(f"{monster_art}")
-    player.weapon_damage = 4
     print(f"You pick the nearest stick (1d{player.weapon_damage}) you can find as your weapon.")
+    player.weapon = Weapon(min_dmg=1, max_dmg=4, weapon_type=WeaponType.STICK, to_hit=0.5, special_effect=SpecialEffect.SLOW, proc=0.5)
     return fight(monsterGenerator.generate_monster(max_hp=10, max_dmg=15), player)
-
-
 
 
 ##############################################################
@@ -259,25 +299,33 @@ def cabin():
                 print("You have already looked inside.")
         if action == "e":
             p(
-                "You step back outside. Suddenly you hear a loud crack and just when you realize it comes from your own skull, everything goes dark.", "bwhite")
+                "You step back outside. Suddenly you hear a loud crack and just when you realize it comes from your own skull, everything goes dark.",
+                "bwhite")
             input("Press enter to continue.")
             return True
+
+
 #################################
 # 5. Jail time
 #################################
 def jail():
-    print("You wake up to distant dripping sound. Looking around you notice you are being held in an old musty dungeon.")
+    pee_in_jail = False
+    guard_unconscious = False
+    print(
+        "You wake up to distant dripping sound. Looking around you notice you are being held in an old musty dungeon.")
     while True:
-        choice = input("What will you do? ")
-        words = choice.split(" ")
+        input_choice = input("What will you do? ")
+        words = input_choice.split(" ")
         if words[0] in ["search", "look", "seek"]:
-            if len(words) <2:
-                p("You look around and see some loose stones, barred window and hear distant snores. The wooden door with small latch shines small amount of light inside.", "bwhite")
+            if len(words) < 2:
+                p("You look around and see some loose stones, barred window and hear distant snores. The wooden door with small latch shines small amount of light inside.",
+                  "bwhite")
                 continue
             if words[1] in ["window", "bars"]:
                 p("The bars feel very strong. No escape through here.", "bwhite")
             elif words[1] in ["door", "latch"]:
-                p("The door seems to be old and slightly rotten. With right tools you might be able to break through.", "bwhite")
+                p("The door seems to be old and slightly rotten. With right tools you might be able to break through.",
+                  "bwhite")
         if words[0] in ["take", "get", "pick"]:
             if len(words) < 2:
                 p("Take what?", "bwhite")
@@ -285,20 +333,39 @@ def jail():
             if words[1] in ["stone", "rock", "pebble"]:
                 p("You put a stone in your pocket.", "bwhite")
                 player.inventory.add_item("stone")
-        if choice in ["pockets", "inventory", "items"]:
+        if input_choice in ["pockets", "inventory", "items"]:
             player.inventory.print_inventory()
+        if words[0] in ["pee", "toilet", "piss"]:
+            p("You take a leak against the door. The pool of liquid slowly freezes. This could work as a trap!",
+              "bwhite")
+            pee_in_jail = True
+        if words[0] in ["knock", "call", "yell"]:
+            if pee_in_jail:
+                guard_unconscious = True
+                p("A guard comes to open the door to see what the commotion is about. They slip on the frozen pee and bang their head, knocking them unconcious.",
+                  "bwhite")
+            else:
+                p("The guard comes in and knocks you on your ass.", "bwhite")
+                p(f"You {random.choice(['twat', 'turdling', 'wanker', 'pimple', 'imbecile', 'snollygoster', 'cunt', 'pillock', 'cum dumpster', 'piss flaps', 'fuckwit', 'twatopotamus', 'cock holster', 'ass wipe', 'dingleberry', 'poo on a stick'])}, what {random.choice(['the hell', 'the fuck', 'in tarnation'])} do you want?",
+                  "orange")
+        if words[0] in ["check"] and guard_unconscious:
+            p("You search the guards body and find a club on them. You take it and sneak out of the dungeon.", "bwhite")
+            player.weapon_damage = 5
     return True
+
+
 def main():
-    if not woods():
-        return
-    if not lost():
-        return
+    # if not woods():
+    #     return
+    # if not lost():
+    #     return
     if not tappelu():
         return
-    if not cabin():
-        return
+    # if not cabin():
+    #     return
     if not jail():
         return
+
 
 while True:
     main()
--- player.py
+++ player.py
@@ -1,4 +1,7 @@
 from inventory import Inventory
+from enum import Enum
+
+
 class Player:
     def __init__(self, hitpoints, weapon_damage, magicpoints):
         self.hitpoints = hitpoints
@@ -7,3 +10,33 @@ class Player:
         self.weapon_damage = weapon_damage
         self.food = 0
         self.inventory = Inventory()
+        self.weapon = None
+
+class WeaponType(Enum):
+    STICK = "stick"
+    STONE = "stone"
+    KNIFE = "knife"
+    DAGGER = "dagger"
+    SWORD = "sword"
+    LONG_SWORD = "long sword"
+    BATTLE_AXE = "battle axe"
+    CLUB = "club"
+    CROSSBOW = "crossbow"
+    DILDO_BAZOOKA = "dildo bazooka"
+
+
+class SpecialEffect(Enum):
+    BLEED = "bleed"
+    SLOW = "slow"
+    STUN = "stun"
+
+
+class Weapon:
+
+    def __init__(self, min_dmg, max_dmg, weapon_type, to_hit, special_effect=None, proc=0):
+        self.min_dmg = min_dmg
+        self.max_dmg = max_dmg
+        self.weapon_type = weapon_type
+        self.special_effect = special_effect
+        self.proc = proc
+        self.to_hit = to_hit
